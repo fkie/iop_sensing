65d41d89d960890b2af1126b3eb1d03545d1c761
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2017-05-19 13:57:46 +0200

    updated to new PluginInterface

--- iop_costmap2d_fkie/src/CostMap2DPlugin_1_0.cpp
+++ iop_costmap2d_fkie/src/CostMap2DPlugin_1_0.cpp
@@ -38,32 +38,16 @@ CostMap2DPlugin_1_0::CostMap2DPlugin_1_0()
 	p_transport_service = NULL;
 }
 
-CostMap2DPlugin_1_0::~CostMap2DPlugin_1_0()
-{
-
-}
-
-JTS::Service* CostMap2DPlugin_1_0::get_iop_service()
+JTS::Service* CostMap2DPlugin_1_0::get_service()
 {
 	return p_my_service;
 }
 
-const std::type_info & CostMap2DPlugin_1_0::get_iop_service_type()
+void CostMap2DPlugin_1_0::create_service(JTS::JausRouter* jaus_router)
 {
-	return typeid(CostMap2DService);
-}
-
-const std::type_info & CostMap2DPlugin_1_0::get_base_service_type()
-{
-	return typeid(AccessControlService);
-}
-
-
-void CostMap2DPlugin_1_0::create_jts_service(JTS::JausRouter* jaus_router)
-{
-	p_base_service = dynamic_cast<AccessControlService *>(get_base_service());
-	p_events_service = dynamic_cast<EventsService *>(get_base_service(2));
-	p_transport_service = dynamic_cast<TransportService *>(get_base_service(3));
+	p_base_service = static_cast<AccessControlService *>(get_base_service());
+	p_events_service = static_cast<EventsService *>(get_base_service(2));
+	p_transport_service = static_cast<TransportService *>(get_base_service(3));
 	p_my_service = new CostMap2DService(jaus_router, p_transport_service, p_events_service, p_base_service);
 }
 
